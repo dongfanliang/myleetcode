46596c0e5ba04b749cadd9c3edd5d6e4adbc4297
dongfanliang <dongfanliang@zuoyebang.com>
2021-02-23 10:26:50 +0800

    add tree

---
+++ tree/traversal.go
@@ -0,0 +1,72 @@
+package tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+* inorder: https://leetcode-cn.com/problems/binary-tree-inorder-traversal/submissions/
+* preorder: https://leetcode-cn.com/problems/binary-tree-preorder-traversal/submissions/
+* postorder: https://leetcode-cn.com/problems/binary-tree-postorder-traversal/submissions/
+**/
+
+// 递归方式
+func inorderD(root *TreeNode) []int {
+	// 左 -> 根 -> 右
+	res := []int{}
+
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		inorder(root.Left)
+		res = append(res, root.Val)
+		inorder(root.Right)
+	}
+
+	inorder(root)
+	return res
+}
+
+func preorderD(root *TreeNode) []int {
+	// 根 -> 左 -> 右
+	res := []int{}
+
+	var preorder func(root *TreeNode)
+	preorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		res = append(res, root.Val)
+		preorder(root.Left)
+		preorder(root.Right)
+	}
+	preorder(root)
+
+	return res
+}
+
+func postorderD(root *TreeNode) []int {
+	// 左 -> 右 -> 根
+	res := []int{}
+
+	var postorder func(root *TreeNode)
+	postorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		postorder(root.Left)
+		postorder(root.Right)
+		res = append(res, root.Val)
+	}
+
+	postorder(root)
+	return res
+}
+
+// 迭代方式
